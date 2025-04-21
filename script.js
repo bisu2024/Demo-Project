9f8e1b4901df3249f673e184330fe5789b957939
bisu2024 <bisu26.cstrrk@gmail.com>
2025-04-21 21:39:12 +0530

    Copy functionality added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const heading = document.getElementById('heading');
+const copyIcon = document.getElementById('copy-icon');
 
 heading.addEventListener('hover', () => {
     heading.style.color = 'green';
@@ -7,3 +8,14 @@ heading.addEventListener('hover', () => {
 window.addEventListener('load', () => {
     window.alert("You are browsing");
 });
+
+copyIcon.addEventListener('click', () => {
+    navigator.clipboard.writeText(heading.textContent);
+    copyIcon.classList.remove('fa-light');
+    copyIcon.classList.add('fa-solid');
+
+    setTimeout(() => {
+        copyIcon.classList.remove('fa-solid');
+        copyIcon.classList.add('fa-light');
+    })
+})
