54b46543163329010d34c756bb21d9135bc70a37
bisu2024 <bisu26.cstrrk@gmail.com>
2025-04-21 21:22:32 +0530

    Added big feature

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const heading = document.getElementById('heading');
 heading.addEventListener('hover', () => {
     heading.style.color = 'green';
 })
+
+window.addEventListener('load', () => {
+    window.alert("You are browsing");
+});
